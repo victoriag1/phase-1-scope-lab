24361a6a5809bf0fbaa266d33ef19b94022341a7
Victoria Gonzalez <gonzalezevic@gmail.com>
2022-02-08 19:06:37 -0800

    commit tree 4c7ae95a8e2ceab459742b614740008e4acabc2a

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Write your solution in this file!
+var customerName = 'bob'
+const leastFavoriteCustomer = 'vic'
+
+function upperCaseCustomerName(){
+    return customerName =customerName.toUpperCase()
+}
+
+function setBestCustomer(){
+    return  bestCustomer = 'not bob'
+}
+
+function overwriteBestCustomer(){
+    bestCustomer = 'maybe bob'
+}
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer= 'james'
+}
